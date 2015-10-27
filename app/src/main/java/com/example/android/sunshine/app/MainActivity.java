040604cfe78c7f0030e270c277f0f71995a577b0
Carlos Lamb <thebiggcaok@gmail.com>
2015-10-27 20:26:40 +0100

    Added ASyncTask

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -1,19 +1,9 @@
 package com.example.android.sunshine.app;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -24,7 +14,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
     }
@@ -54,49 +44,7 @@ public class MainActivity extends ActionBarActivity {
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
-        ArrayAdapter<String> mForecastAdapter;
 
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-
-            // Create some dummy data for the ListView.  Here's a sample weekly forecast
-            String[] data = {
-                    "Today - Sunny - 88/63",
-                    "Tomorrow - Foggy - 88/63",
-                    "Weds - Foggy - 88/63",
-                    "Thurs - Sunny - 88/63",
-                    "Fri - Rainy - 63/63",
-                    "Sat - Sunny - 26/63",
-                    "Mon - Sunny - 36/63",
-                    "Mon - Rainy - 66/63",
-                    "Mon - Foggy - 66/63",
-                    "Mon - Sunny - 44/63"
-            };
-
-            List<String> forecast = new ArrayList<String>(Arrays.asList(data));
-
-
-            // Now that we have some dummy forecast data, create an ArrayAdapter.
-            // The ArrayAdapter will take data from a source (like our dummy forecast) and
-            // use it to populate the ListView it's attached to.
-            mForecastAdapter = new ArrayAdapter<String>(getActivity(),
-                    R.layout.list_item_forecast,
-                    R.id.list_item_forecast_textview,
-                    forecast);
-
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-            // Get a reference to the ListView, and attach this adapter to it.
-            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
-            listView.setAdapter(mForecastAdapter);
-
-            return rootView;
-        }
 
 
     }
@@ -104,4 +52,4 @@ public class MainActivity extends ActionBarActivity {
 
 
 
-}
+
